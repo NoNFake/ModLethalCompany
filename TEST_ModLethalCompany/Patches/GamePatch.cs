77f19f37c0648f2e54a112833b3a2e11e8149e5e
Restored [.-.] <86575196+NoNFake@users.noreply.github.com>
2024-01-15 22:53:21 +0200

    Add support LethalConfig

---
+++ TEST_ModLethalCompany/Patches/GamePatch.cs
@@ -0,0 +1,48 @@
+﻿// /Patches/Sprint.cs
+
+using BepInEx.Logging;
+using GameNetcodeStuff;
+using HarmonyLib;
+using MiNET.Plugins;
+using ModLethalCompany;
+
+namespace ModLethalCompany.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+
+
+    internal class GamePatch
+    {
+
+        /*[HarmonyPatch(nameof(PlayerControllerB.))]*/
+        [HarmonyPostfix, HarmonyPatch("Update")]
+        static void sprintPatch(ref bool ___takingFallDamage, ref float ___sprintMeter, ref float ___carryWeight, ref float ___climbSpeed)
+        {
+            // float
+            ___sprintMeter = Plugin.sprint.Value;
+            ___carryWeight = 0.5f;
+            ___climbSpeed = Plugin.climb_speed.Value;
+
+            // bool
+            ___takingFallDamage = Plugin.fall_damage.Value;
+        } // Update
+
+
+        [HarmonyPatch("Start")]
+        [HarmonyPrefix]
+        static void getNightVision(ref PlayerControllerB __instance)
+        {
+
+            Plugin.playerRef = __instance;
+            bool nightVisionEnabled = Plugin.nightVision.Value;
+            float nightVisionIntensity = Plugin.nightVisionIntensity.Value;
+            float nightVisionRange = Plugin.nightVisionRange.Value;
+
+            Plugin.playerRef.nightVision.color = UnityEngine.Color.white;
+            Plugin.playerRef.nightVision.intensity = nightVisionIntensity;
+            Plugin.playerRef.nightVision.range = nightVisionRange;
+
+            Plugin.playerRef.nightVision.enabled = nightVisionEnabled;
+        }
+    }
+}
--- TEST_ModLethalCompany/Plugin.cs
+++ TEST_ModLethalCompany/Plugin.cs
@@ -4,78 +4,91 @@ using BepInEx;
 using BepInEx.Logging;
 using GameNetcodeStuff;
 using HarmonyLib;
-using System;
-using System.Runtime.CompilerServices;
+using UnityEngine;
+// using TEST_ModLethalCompany.Patches;
+using BepInEx.Configuration;
+using LethalConfig;
 using ModLethalCompany.Patches;
-using MiNET.Effects;
+using LethalConfig.ConfigItems.Options;
+using LethalConfig.ConfigItems;
 
 namespace ModLethalCompany
 {
     [BepInPlugin(modGUID, modName, modVersion)]
+    [BepInDependency("ainavt.lc.lethalconfig")]
     public class Plugin : BaseUnityPlugin
     {
-
-
-
-
-
-
-        private const string modGUID = "UiX.LC";
-        private const string modName = "UiX";
-        private const string modVersion = "1.0.0.4";
+        private const string modGUID = "nonfake.LC_Modify";
+        private const string modVersion = "1.0.1.4";
+        private const string modName = $"{modVersion}r_UiX";
+        
 
         private readonly Harmony harmony = new Harmony(modGUID);
 
 
         internal static PlayerControllerB? playerRef;
         private static Plugin? Instance;
+
         internal ManualLogSource? mls;
 
 
+        // Any | floats
+        internal static ConfigEntry<float> sprint;
+        internal static ConfigEntry<float> climb_speed;
+
+        // Any | bool
+        internal static ConfigEntry<bool> fall_damage;
+
+
+        // night vision
         // bool
-        internal static bool nightVision = false;
-        internal static bool SetNightVisionEnabled = false;
-        internal static bool isPlayerControlled = false;
-        internal static bool isPlayerDead = false;
-        internal static bool IsInspectingItem = false;
-        internal static bool twoHanded = false;
-        internal static bool twoHandedAnimation = false;
-        internal static bool isHoldingObject = true;
-        /*
-         
-         public bool twoHanded;
+        internal static ConfigEntry<bool> nightVision;
 
-	        public bool twoHandedAnimation;
-         */
+        // float 
 
-        // float
-        internal static float nightVisionIntensity = 0f;
-        internal static float nightVisionRange = 0f;
-        internal static float carryWeight = 1f; // 
-        internal static float carryRate = 1f;
-        internal static float carryRateMultiplier = 1f;
-        internal static float targetFOV = 66f;
-        internal static float climbSpeed = 4f;
-
-        // 
-        internal static UnityEngine.Color nightVisionColor = UnityEngine.Color.black;
+        internal static ConfigEntry<float> nightVisionIntensity;
+        internal static ConfigEntry<float> nightVisionRange;
 
 
         void Awake()
         {
-            if (Instance == null) { Instance = this; }
+
+            if (Instance == null ) { Instance = this; }
+
+            // Any | Float
+            sprint = Config.Bind("Sprint", "Infinity sprint", 0f, "");
+            climb_speed = Config.Bind("Sprint", "Climb speed example 15", 0f, "This may cause major amounts of lag. use at your own risk");
+
+            fall_damage = Config.Bind("Fall Damage", "No Damage when falling", true, "This may cause major amounts of lag. use at your own risk");
+
+
+            // Night vision | bools
+            nightVision = Config.Bind("Night Vision", "Enable night vision", false, "This may cause major amounts of lag. use at your own risk");
+
+            // fliat
+            nightVisionIntensity = Config.Bind("Night Vision", "Intensity of night vision", 0f, "This may cause major amounts of lag. use at your own risk");
+            var VisionIntensity_Slider = new FloatSliderConfigItem(nightVisionIntensity, new FloatSliderOptions {  Min = 1f, Max = 9000f });
+            LethalConfigManager.AddConfigItem(VisionIntensity_Slider);
+
+            nightVisionRange = Config.Bind("Night Vision", "Range of night vision", 0f, "This may cause major amounts of lag. use at your own risk");
+            var nightVisionRange_Slider = new FloatSliderConfigItem(nightVisionRange, new FloatSliderOptions {  Min = 1f, Max = 9000f });
+            LethalConfigManager.AddConfigItem(nightVisionRange_Slider);
+
+
+
 
             mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
 
-            mls.LogInfo("The test mod has started");
+            mls.LogInfo($"<================================================>\n\n\t|The {modName} has started|\n\n<================================================>");
 
             harmony.PatchAll(typeof(Plugin));
-            harmony.PatchAll(typeof(PlayerControllerBPatch));
+            harmony.PatchAll(typeof(GamePatch));
+
         }
 
         internal static void WaitForSeconds(float v)
         {
             throw new NotImplementedException();
         }
-    }
+    } // Plugin
 }
